fc5e72c704ccfe1d2d51a55eb17491d21d158b87
Quenton Cook <quenton.cook@gmail.com>
2018-09-30 17:13:33 -0700

    Update components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return <Text>I love boopsie!</Text>;
+  }
+}
